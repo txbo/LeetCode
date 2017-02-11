0825c1063aba87dbb184986864b15b7b8a4366c8
grassrootscoder <919548783@qq.com>
2017-02-11 09:30:33 +0800

    update 217

--- 217 Contains Duplicate.js
+++ 217 Contains Duplicate.js
@@ -6,3 +6,34 @@
 var containsDuplicate = function(nums) {
   return Array.from(new Set(nums)).length !== nums.length
 };
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  nums.sort()
+  for (var i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true
+    }
+  }
+  return false
+};
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+  var temp = {}
+  var len = nums.length
+  for (var i = 0; i < len; i++) {
+    if (nums[i] in temp) {
+      return true
+    } else {
+      temp[nums[i]] = true
+    }
+  }
+  return false
+};
