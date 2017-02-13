a98b71d509fc1eb13eab61a3ca4c240a2c5b93c7
grassrootscoder <919548783@qq.com>
2017-02-13 11:22:05 +0800

    206

--- 206 Reverse Linked List.js
+++ 206 Reverse Linked List.js
@@ -10,7 +10,34 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {
+var reverseList = function(head) {
+  var m = head,
+    n
+  if (m === null) {
+    return head
+  }
+  while (m.next !== null) {
+    n = m.next
+    m.next = n.next
+    n.next = head
+    head = n
+  }
+  return head
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
   var arr = []
   rList(head)
   return arr
